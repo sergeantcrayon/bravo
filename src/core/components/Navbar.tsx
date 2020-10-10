2305ac87dbfb71ac317106ef434c1a501f215bd5
Victor Bravo <vicbravofreelancer@gmail.com>
2020-10-09 23:56:34 -0400

    logo

--- src/core/components/Navbar.tsx
+++ src/core/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
 import './Navbar.scss';
 import Login from './Login';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <Fragment>
       <div className="bar-container">
         <div className="half-bar">
-          <div className="nav-logo">Bravo</div>
+          <Link to={`/`}>
+            <span className="nav-logo">Bravo</span>
+          </Link>
           <Login />
         </div>
       </div>
